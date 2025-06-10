f5ca324d88847e438764433a7be27db5c15d19c8
Sri Ganesh Shiramshetty <115027993+GaneshVarma1@users.noreply.github.com>
2025-06-09 19:25:06 -0500

    fix: update theme provider implementation

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ReactNode } from "react";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-interface ThemeProviderProps {
-  children: ReactNode;
-}
-
-export function ThemeProvider({ children }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      {...props}
+    >
       {children}
     </NextThemesProvider>
   );
