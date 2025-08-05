1324897c3754333297a52d017abad3fe7d927cfb
nikith99 <86515044+nikith99@users.noreply.github.com>
2025-08-05 16:42:06 -0500

    Update footer.ts

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -12,9 +12,9 @@
 export const footerData = {
   // Social Media Links
   social: {
-    email: "mailto:nikith290699@gmail.com",
-    github: "https://github.com/nikith290699",
-    linkedin: "https://www.linkedin.com/in/sri-nikith-11b92036a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BtIjq7SDOQ7C4xGzS9D1UZw%3D%3D"
+    email: "mailto:psrinikith05@gmail.com",
+    github: "https://github.com/nikith99",
+    linkedin: "https://www.linkedin.com/in/poddaturi-sri-nikith-0398b5183?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B2LXOk8TQStu%2BMXgyFLWNHQ%3D%3D"
   },
 
   // Footer Message (can include HTML if needed)
