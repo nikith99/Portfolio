f78a376385b7be13e6383ccd9853060a93167b10
nikith290699-glitch <nikith290699@gmail.com>
2025-08-05 14:07:50 -0500

    Update footer.ts

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -12,9 +12,9 @@
 export const footerData = {
   // Social Media Links
   social: {
-    email: "mailto:your.email@example.com",
-    github: "https://github.com/yourusername",
-    linkedin: "https://linkedin.com/in/yourusername"
+    email: "mailto:nikith290699@gmail.com",
+    github: "https://github.com/nikith290699",
+    linkedin: "https://www.linkedin.com/in/sri-nikith-11b92036a?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BtIjq7SDOQ7C4xGzS9D1UZw%3D%3D"
   },
 
   // Footer Message (can include HTML if needed)
